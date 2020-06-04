8771b5a348729fca06dfd77c809030868409769c
Paolo Arcagni <paolo.arcagni@gmail.com>
2020-06-04 17:18:27 +0200

    Prometheus reconfigured

--- ansible/playbooks/files/k8s-files/bigip-k8s-ctlr/1.prework-f5bigip.sh
+++ ansible/playbooks/files/k8s-files/bigip-k8s-ctlr/1.prework-f5bigip.sh
@@ -2,5 +2,6 @@
 
 kubectl create secret generic bigip-login -n kube-system --from-literal=username=admin --from-literal=password=Default1234!
 kubectl create serviceaccount k8s-bigip-ctlr -n kube-system
+kubectl create clusterrolebinding k8s-bigip-ctlr-clusteradmin --clusterrole=cluster-admin --serviceaccount=kube-system:k8s-bigip-ctlr
 
 kubectl create -f 2.bigip-k8s-ctlr-deployment.yaml
