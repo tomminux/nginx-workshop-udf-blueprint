54bc1dca2070ca2f5e7802bc7486b5ced1d87d3b
Paolo Arcagni <tomminux@users.noreply.github.com>
2020-10-23 19:06:50 +0200

    Update infra-server-postReboot.sh

--- init-scripts/infra-server-postReboot.sh
+++ init-scripts/infra-server-postReboot.sh
@@ -12,4 +12,11 @@ sudo apt install dnsmasq -y
 sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
 sudo rm /etc/resolv.conf
+sudo sh -c 'cat <<EOF > /etc/resolv.conf
+search f5-udf.com
+nameserver 10.1.1.4
+nameserver 10.1.1.2
+nameserver 8.8.8.8
+EOF'
+
 sudo systemctl restart dnsmasq
