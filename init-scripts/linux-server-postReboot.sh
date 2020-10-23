2af46d703a5e33ccc9d51e2597fb2f399c664e48
Paolo Arcagni <tomminux@users.noreply.github.com>
2020-10-23 19:06:17 +0200

    Create linux-server-postReboot.sh

---
+++ init-scripts/linux-server-postReboot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common xubuntu-desktop
+
+## ..:: Installing dnsmasq ::..
+sudo rm /etc/resolv.conf
+sudo sh -c 'cat <<EOF > /etc/resolv.conf
+search f5-udf.com
+nameserver 10.1.1.4
+nameserver 10.1.1.2
+nameserver 8.8.8.8
+EOF'
+
