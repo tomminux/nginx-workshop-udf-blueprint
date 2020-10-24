016c020e9681d5483c263e6e79edfec0d307f44d
Paolo Arcagni <tomminux@users.noreply.github.com>
2020-10-24 08:03:32 +0200

    Update linux-jumphost-postReboot.sh

--- init-scripts/linux-jumphost-postReboot.sh
+++ init-scripts/linux-jumphost-postReboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common xrdp xfce4 firefox
 
 ## ..:: Setting DNS Resolution for this UDF Deployment ::..
 sudo rm /etc/resolv.conf
@@ -25,4 +25,36 @@ nameserver 10.1.1.4
 nameserver 10.1.1.2
 nameserver 8.8.8.8
 EOF'
+
+## ..:: Configuring XFCE4 Desktop Environment ::..
+
+echo xfce4-session > /home/ubuntu/.xsession
+sudo cp /etc/xrdp/startwm.sh /etc/xrdp/startwm.sh-bak
+
+sudo sh -c 'cat <<EOF > /etc/xrdp/startwm.sh
+#!/bin/sh
+
+if [ -r /etc/default/locale ]; then
+  . /etc/default/locale
+  export LANG LANGUAGE
+fi
+
+startxfce4
+
+EOF'
+
+sudo systemctl enable xrdp
+sudo systemctl restart xrdp
+sudo systemctl status xrdp
+
+## ..:: Google Chrome installation ::..
+
+sudo sh -c 'cat <<EOF > /etc/apt/sources.list.d/google-chrome.list
+deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
+
+EOF'
+wget https://dl.google.com/linux/linux_signing_key.pub
+sudo apt-key add linux_signing_key.pub
+sudo apt update
+sudo apt install google-chrome-stable
 
